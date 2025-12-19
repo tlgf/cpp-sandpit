0f88dcac996213b3eb429dbed12be44cc30974ac
tlgf <6648717+tlgf@users.noreply.github.com>
2025-12-19 21:39:31 +1100

    get some idiomatic c++17 char parsing goodness, from `#include <charconv>`

--- iterators/main.cpp
+++ iterators/main.cpp
@@ -1,11 +1,12 @@
+#include <charconv>
 #include <cstdlib>
 #include <initializer_list>
 #include <iostream>
 #include <memory>
 #include <vector>
-
 int main(int argc, char *argv[]) {
 
+  std::cout << __cplusplus << std::endl;
   if (argc <= 1)
     return EXIT_FAILURE;
 
@@ -15,12 +16,24 @@ int main(int argc, char *argv[]) {
   for (auto i = 1; i < argc; i++) {
     int val;
     printf("user val at index %d is %s\n", i, argv[i]);
+#if 0
     try {
       val = std::stoi(argv[i]);
     } catch (const std::invalid_argument &e) {
       std::cout << "invalid argument: " << argv[i] << std::endl;
-      std::cout << "only accepts integers." return EXIT_FAILURE;
+      std::cout << "only accepts integers." << std::endl;
+      return EXIT_FAILURE;
     }
+#endif
+#if 1
+    auto result = std::from_chars(argv[i], argv[i + 1], val);
+    if (result.ec != std::errc()) {
+      std::cout << "invalid argument: " << argv[i] << std::endl;
+      std::cout << "only accepts integers." << std::endl;
+      return EXIT_FAILURE;
+    }
+#endif
+
     auto pos = args.end();
     args.insert(pos, val);
   }
